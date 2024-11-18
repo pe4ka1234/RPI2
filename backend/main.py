d6b41bfbf29b5122996595bf070e345863ee8c4e
pe4ka1234 <andrey.grushin.00@mail.ru>
2024-11-18 20:30:36 +0300

    virsion2

--- backend/main.py
+++ backend/main.py
@@ -22,4 +22,5 @@ routers.init_app(app)
 
 if __name__ == '__main__':
     
-    uvicorn.run('main:app', host=settings.HOST, port=settings.PORT, reload=True)
+    uvicorn.run('main:app', host=127.0.0.1, port=5000, reload=True)
+    
